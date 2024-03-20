4c939fb1e4c845cd6586ef1a4c4b9c46dde7f31d
Chaitanya Joshi <36196744+chaitanyaj14@users.noreply.github.com>
2024-03-20 23:32:30 +0530

    Add Map Class (#8)

--- geoquanta/__init__.py
+++ geoquanta/__init__.py
@@ -3,3 +3,5 @@
 __author__ = """Chaitanya Joshi"""
 __email__ = "chaitanyaj14@gmail.com"
 __version__ = "0.0.2"
+
+from .geoquanta import *
--- geoquanta/geoquanta.py
+++ geoquanta/geoquanta.py
@@ -1,1 +1,9 @@
 """Main module."""
+
+import ipyleaflet
+
+class Map(ipyleaflet.Map):
+
+    def __init__(self, center=[22, 79], zoom=5, **kwargs):
+        super().__init__(center=center, zoom=zoom, **kwargs)
+        self.add_control(ipyleaflet.LayersControl())
