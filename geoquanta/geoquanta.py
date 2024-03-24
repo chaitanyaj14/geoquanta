347781d4cb8c6cc8dd39ee069786535f974fc900
chaitanyaj14 <chaitanyaj14@gmail.com>
2024-03-24 18:35:35 +0530

    add vector geojson shp and data

--- geoquanta/geoquanta.py
+++ geoquanta/geoquanta.py
@@ -5,7 +5,7 @@ from ipyleaflet import basemaps
 
 class Map(ipyleaflet.Map):
 
-    def __init__(self, center=[22, 79], zoom=5, **kwargs):
+    def __init__(self, center=[22, 79], zoom=4, **kwargs):
         super().__init__(center=center, zoom=zoom, **kwargs)
         self.add_control(ipyleaflet.LayersControl(position='topright'))
 
@@ -19,3 +19,36 @@ class Map(ipyleaflet.Map):
             self.add_tile_layer(url, name)
         else:
             self.add(name)
+
+    def add_geojson(self, data, name="geojson", **kwargs):
+        
+        import json
+
+        if isinstance (data, str):
+            with open(data, 'r') as data:
+                data = json.load(data)
+
+        if "style" not in kwargs:
+            kwargs["style"] = {"color": "blue", 'fillOpacity': 0.2, 'weight': 1}
+
+        if "hover_style" not in kwargs:
+            kwargs["hover_style"] = {'color': 'white', 'fillOpacity': 0.6}
+
+        layer = ipyleaflet.GeoJSON(data=data, name=name, **kwargs)
+        self.add(layer)
+
+    def add_shapefile(self, data, name="shp", **kwargs):
+
+        import shapefile
+
+        if isinstance (data, str):
+            with shapefile.Reader(data) as shp:
+                data = shp.__geo_interface__
+
+        if "style" not in kwargs:
+            kwargs["style"] = {"color": "blue", 'fillOpacity': 0.2, 'weight': 1}
+
+        if "hover_style" not in kwargs:
+            kwargs["hover_style"] = {'color': 'white', 'fillOpacity': 0.6}
+
+        self.add_geojson(data, name, **kwargs)
