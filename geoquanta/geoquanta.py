e32cfd22d8358163e4c6f8b01bb7ffe91747fd78
chaitanyaj14 <chaitanyaj14@gmail.com>
2024-04-16 23:14:26 +0530

    add raster data

--- geoquanta/geoquanta.py
+++ geoquanta/geoquanta.py
@@ -16,6 +16,10 @@ class Map(ipyleaflet.Map):
             center (list, optional): Set the center of the map. Defaults to [22, 79].
             zoom (int, optional): Set the zoom level of the map. Defaults to 4.
         """
+        
+        if "scroll_wheel_zoom" not in kwargs:
+            kwargs["scroll_wheel_zoom"] = True
+        
         super().__init__(center=center, zoom=zoom, **kwargs)
         self.add_control(ipyleaflet.LayersControl(position='topright'))
 
@@ -97,3 +101,43 @@ class Map(ipyleaflet.Map):
             kwargs["hover_style"] = {'color': 'white', 'fillOpacity': 0.6}
 
         self.add_geojson(data, name, **kwargs)
+        
+    def add_image(self, url, bounds, name='image', **kwargs):
+        """
+        Adds an image overlay to the map.
+
+        Args:
+        - url (str): The URL of the image.
+        - bounds (tuple): A tuple of the form (south, west, north, east) representing the bounds of the image overlay.
+        - name (str, optional): The name of the image overlay. Default is 'image'.
+        - **kwargs: Additional keyword arguments accepted by ipyleaflet.ImageOverlay.
+        """
+        layer = ipyleaflet.ImageOverlay(url=url, bounds=bounds, name=name, **kwargs)
+        self.add(layer)
+        
+    def add_raster (self, data, name="raster", zoom_to_layer=True, **kwargs):
+        """
+        Add a raster layer to the map.
+
+        Args:
+            data: The raster data to be added to the map.
+            name (str): The name of the raster layer (default is "raster").
+            zoom_to_layer (bool): Whether to zoom to the added layer (default is True).
+            **kwargs: Additional keyword arguments to be passed.
+
+        Raises:
+            ImportError: If the localtileserver package is not installed.
+        """
+        try:
+            from localtileserver import TileClient, get_leaflet_tile_layer
+        except ImportError:
+            raise ImportError("Please install the localtileserver package.")
+        
+        client = TileClient(data)
+        layer = get_leaflet_tile_layer(client, name=name)
+        self.add(layer)
+        
+        if zoom_to_layer:
+            self.center = client.center()
+            self.zoom = client.default_zoom
+    
